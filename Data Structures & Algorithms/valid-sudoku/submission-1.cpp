147d2db40981f23ef9f32ec59d1c55d47804c352
Teo Berguerre <48836252+TeoBerguerre@users.noreply.github.com>
2026-08-20 19:12:07 +0200

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int boardSize = board.size();
+        if (board.size() != 9) {
+            return false;
+        }
+
+        for (int row = 0; row < boardSize; row++) {
+            unordered_set<char> seen;
+            for (int i = 0; i < boardSize; i++) {
+                if (board[row][i] == '.') continue;
+                if (seen.count(board[row][i])) return false;
+                seen.insert(board[row][i]);
+            }
+        }
+
+        for (int col = 0; col < boardSize; col++) {
+            unordered_set<char> seen;
+            for (int i = 0; i < boardSize; i++) {
+                if (board[i][col] == '.') continue;
+                if (seen.count(board[i][col])) return false;
+                seen.insert(board[i][col]);
+            }
+        }
+
+        for (int square = 0; square < boardSize; square++) {
+            std::unordered_set<char> seen;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int row = (square / 3) * 3 + i;
+                    int col = (square % 3) * 3 + j;
+                    if (board[row][col] == '.') {
+                        continue;
+                    }
+                    if (seen.count(board[row][col])) {
+                        return false;
+                    } else {
+                        seen.insert(board[row][col]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
